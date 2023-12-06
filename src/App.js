cecda7afd269a225a2a8b60d969973aaaa0b819f
Ruilin P's ArchLinux <ruilin.peng@mail.utoronto.ca>
2023-12-06 04:40:59 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PetList from './components/pets';
 import SinglePetInfo from './components/pet';
 import Landing from './pages/landing';
-import Login from './components/login'
-import ShelterSignup from './components/ShelterSignup'
-import SeekerSignup from './components/SeekerSignup';
-import SearchPets from './components/Searchpets';
+// import Login from './components/login'
+// import ShelterSignup from './components/ShelterSignup'
+// import SeekerSignup from './components/SeekerSignup';
+// import SearchPets from './components/Searchpets';
 
-import ShelterReviews from './pages/CommentPages/ShelterCommentPage';
+// import ShelterReviews from './pages/CommentPages/ShelterCommentPage';
 import { UserProvider } from './contexts/UserContext';
 import NotFound from './pages/404';
 
@@ -22,11 +21,11 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/pets/" element={<PetList />} />
         <Route path="/pets/:petId" element={<SinglePetInfo />} />
-        <Route path="/login/" element={<Login />} />
+        {/* <Route path="/login/" element={<Login />} />
         <Route path="/signup-shelter" element={<ShelterSignup />} />
         <Route path="/signup-seeker" element={<SeekerSignup />} />
         <Route path="/search-pets" element={<SearchPets/>} />
-        <Route path="/shelters/:shelterId/comments/" element={<ShelterReviews/>} />
+        <Route path="/shelters/:shelterId/comments/" element={<ShelterReviews/>} /> */}
         <Route path="/404/" element={<NotFound/>} />
       </Routes>
     </BrowserRouter>
